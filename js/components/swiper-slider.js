8518b65a52a39f8cd61f25d38b0d8692d059cfc0
makrenka <smokrenko79@gmail.com>
2022-07-25 21:52:02 +0300

    add styles for courses slider

--- js/components/swiper-slider.js
+++ js/components/swiper-slider.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.main__articles-slider-list', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -22,17 +22,17 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-new Swiper('.swiper-2', {
+new Swiper('.main__courses-slider-list', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-  slidesPerView: 4,
+  slidesPerView: 2,
   spaceBetween: 30,
   width: 1230,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination-2',
+    el: '.swiper-pagination',
     dynamicBullets: true,
     dynamicMainBullets: 4,
     clickable: true,
