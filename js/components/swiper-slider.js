0519986dae5b3331ee73eba45b2ef46d856fa53c
makrenka <smokrenko79@gmail.com>
2022-08-24 19:49:16 +0300

    edit style for article slider

--- js/components/swiper-slider.js
+++ js/components/swiper-slider.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.main__articles-slider-list', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
+  loop: true,  
   
   breakpoints: {
     1255: {
@@ -61,7 +61,7 @@ const swiper = new Swiper('.main__articles-slider-list', {
     el: '.swiper-pagination',
     dynamicBullets: true,
     dynamicMainBullets: 4,
-    clickable: true,
+    clickable: true,    
   },
 
   // Navigation arrows
